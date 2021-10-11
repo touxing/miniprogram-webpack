9e58a252ebb12899cf3977732bda3dc849c8dc55
hotsuitor <hotsuitor@qq.com>
2021-10-11 09:11:16 +0800

    opt: 多环境变量配置

--- src/env/env.dev.js
+++ src/env/env.dev.js
@@ -1,18 +1,10 @@
-let apiPrex = '/fun'
-let baseUrl = 'http://tes2.api.app.ligumall.com'
 let env = {
-  doamin: baseUrl,
-  apiPrex,
-  spikeBaseUrl: 'http://funshz.cmbc.com', // todo: 秒杀使用单独的接口，未使用，未测试
-  imagePrexDoamin: `${baseUrl}/fun/yt/images/wx`,
-  loginDoamin: baseUrl, // 登录专用
-  webPreDoamin: baseUrl + apiPrex,
   api: 'https://api.xxxxxx.com',
   appId: 'xxxx',
   appIdGzh: 'xxxxx',
   appIdCircel: 'xxxx',
   webPath: 'https://wx-dev.xxxx',
-  value: 'prod',
+  value: 'dev',
   circleAppId: 'xxxxx',
   staticPath: 'xxxxxxx',
 }
--- src/env/env.prod.js
+++ src/env/env.prod.js
@@ -1,17 +1,9 @@
-let apiPrex = '/fun'
-let baseUrl = 'https://weixinshz.cmbc.com.cn'
 let env = {
-  doamin: baseUrl,
-  apiPrex,
-  spikeBaseUrl: 'http://funshz.cmbc.com', // todo: 秒杀使用单独的接口，未使用，未测试
-  imagePrexDoamin: `${baseUrl}/fun/yt/images/wx`,
-  loginDoamin: baseUrl, // 登录专用
-  webPreDoamin: baseUrl + apiPrex,
   api: 'https://api.xxxxxx.com',
   appId: 'xxxx',
   appIdGzh: 'xxxxx',
   appIdCircel: 'xxxx',
-  webPath: 'https://wx-dev.xxxx',
+  webPath: 'https://wx-prod.xxxx',
   value: 'prod',
   circleAppId: 'xxxxx',
   staticPath: 'xxxxxxx',
